46870b819e77e77e7a3307619a1cb807a88f56a0
thinhlv <thinhluuvan@admicro.vn>
2020-02-13 15:37:18 +0700

    hahaha

---
+++ phpMyAdmin/tmp/twig/5b/5b10c1616c3e41e2b3e57ba49a64f912e9d243a21240f5c7a3c4f6870574292e.php
@@ -0,0 +1,45 @@
+<?php
+
+/* config/form_display/form_bottom.twig */
+class __TwigTemplate_6354f2aef06c15b457dd81c2e45c47fbacfb103a873aa4e163ebb6632b96c017 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "config/form_display/form_bottom.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "config/form_display/form_bottom.twig", "E:\\xampp\\phpMyAdmin\\templates\\config\\form_display\\form_bottom.twig");
+    }
+}
